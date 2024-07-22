3fa27029518000c1adad523c62867629e6e1e4db
wulitina <tinamazing.yan.xu@gmail.com>
2024-07-22 21:59:23 +0100

    sign up & sign in & logout

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,7 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.signin,name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Profile
 from django.http import HttpResponse
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already registered')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already registered')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+
+                # log user in and redirect to setting page
+
+                # create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user = user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Username OR password (Credential) is incorrect')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+
