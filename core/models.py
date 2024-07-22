2cf5972ca663aad59465f3734b12ef7766dc2e79
wulitina <tinamazing.yan.xu@gmail.com>
2024-07-22 23:32:22 +0100

    Account Settings

--- core/models.py
+++ core/models.py
@@ -8,8 +8,8 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='wink.jpg')
-    location = models.CharField(max_length=100,blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
