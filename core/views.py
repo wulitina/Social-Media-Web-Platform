25140c901608be3fa410a63ec20c7669dd46c8a2
wulitina <tinamazing.yan.xu@gmail.com>
2024-07-23 16:33:59 +0100

    Upload Post

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.http import HttpResponse
 
 
@@ -10,7 +10,26 @@ from django.http import HttpResponse
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, caption=caption, image=image)
+        new_post.save()
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
+
 
 
 @login_required(login_url='signin')
