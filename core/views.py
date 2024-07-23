1d9a789df9e986184e20112c32ca2d276b611121
wulitina <tinamazing.yan.xu@gmail.com>
2024-07-23 21:26:31 +0100

    Like Post

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.http import HttpResponse
 
 
@@ -15,12 +15,35 @@ def index(request):
 
     posts = Post.objects.all()
 
-    return render(request, 'index.html', {'user_profile': user_profile,'posts':posts},)
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts}, )
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
+
 
 
 @login_required(login_url='signin')
 def upload(request):
-
     if request.method == 'POST':
         user = request.user.username
         image = request.FILES.get('image_upload')
@@ -32,7 +55,6 @@ def upload(request):
         return redirect('/')
     else:
         return redirect('/')
-
 
 
 @login_required(login_url='signin')
