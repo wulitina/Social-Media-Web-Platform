49c13004499241c6045fdc409ef7fa3e7ac86d7b
wulitina <tinamazing.yan.xu@gmail.com>
2024-07-24 18:26:17 +0100

    follow user

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.http import HttpResponse
 
 
@@ -54,7 +54,23 @@ def like_post(request):
         return redirect('/')
 
 
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
 
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follow = FollowersCount.objects.create(follower=follower, user=user)
+            new_follow.save()
+            return redirect('/profile/'+user)
+
+    else:
+        return redirect('/')
 
 @login_required(login_url='signin')
 def upload(request):
